f2c08acb0f67ee6e73065da2080e73be49a3b234
김성준 <kimsj@innopost.com>
2014-07-03 13:32:50 +0900

    Fix Typo.

--- CheckIsMember.php
+++ CheckIsMember.php
@@ -8,7 +8,7 @@ echo '팝빌 연동회원 가입 여부 확인 테스트'.chr(10).'처리결과 
 
 try
 {
-	$result = $TaxinvoiceService->CheckIsMember('1231212312',$LinkID);
+	$result = $MessagingService->CheckIsMember('1231212312',$LinkID);
 	var_dump($result);
 }
 catch(PopbillException $pe) {
